c7448d778a8fb2a28ce42fb78c145929f1a6b52e
henklam <183881683+henklam@users.noreply.github.com>
2025-01-16 15:56:42 +0000

    jason

--- script.js
+++ script.js
@@ -1,82 +1,60 @@
-var pricesN = [];
-var yearsN = [];
-var pricesM = [];
-var yearsM = [];
-var pricesA = [];
-var yearsA = [];
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
-async function getDataNvidia() {
-    const response = await fetch("nvidia.csv");
-    const data = await response.text();
-    const rows = data.split("\n").slice(1);
-    rows.forEach((elem) => {
-        const row = elem.split(",");
-        const year = row[1];
-        const price = row[5];
-        yearsN.push(year);
-        pricesN.push(parseFloat(price));
+const firebaseConfig = {
+    apiKey: "AIzaSyCrTRA1OqClyqoeF3qtdnH63QT3N9dh0Wg",
+    authDomain: "project-c12c6.firebaseapp.com",
+    projectId: "project-c12c6",
+    storageBucket: "project-c12c6.firebasestorage.app",
+    messagingSenderId: "403135006428",
+    appId: "1:403135006428:web:96e95ea04d8fee9f159619"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+var user = document.getElementById("user");
+var pass = document.getElementById("pass");
+var button = document.getElementById("sign-in");
+
+function getUser() {
+    return user.value    
+}
+function getPass() {
+    return pass.value
+}
+
+function signIn() {
+    let user = getUser();
+    let pass = getPass();
+    signInWithEmailAndPassword(auth, user, pass)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            document.location.href = "graph.html";
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+        });
+}
+
+function authSignOut() {
+    signOut(auth).then(() => {
+        showLoggedOutView();
+    }).catch((error) => {
+        console.error(error.message)
     });
 }
 
-async function getDataMeta() {
-    const response = await fetch("meta.csv");
-    const data = await response.text();
-    const rows = data.split("\n").slice(1);
-    rows.forEach((elem) => {
-        const row = elem.split(",");
-        const year = row[0];
-        const price = row[4];
-        yearsM.push(year);
-        pricesM.push(parseFloat(price));
-    });
+function isLoggedIn() {
+    let user = auth.currentUser;
+    if(user) {
+        return true;
+    }
+    return false;
 }
 
-async function getDataAmazon() {
-    const response = await fetch("amazon.csv");
-    const data = await response.text();
-    const rows = data.split("\n").slice(1);
-    rows.forEach((elem) => {
-        const row = elem.split(",");
-        const year = row[0];
-        const price = row[4];
-        yearsA.push(year);
-        pricesA.push(parseFloat(price));
-    });
-}
+button.addEventListener("click", signIn);
 
-function createChart() {
-    const ctx = document.getElementById('myChart');
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: yearsN,
-            datasets: [{
-                label: 'NVIDIA',
-                data: pricesN,
-                borderWidth: 1
-            },{
-                label: 'META',
-                data: pricesM,
-                borderWidth: 1
-            },{
-                label: 'AMAZON',
-                data: pricesA,
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: false 
-                }
-            }
-        }
-    });
-}
-getDataNvidia()
-.then(getDataMeta)
-.then(getDataAmazon)
-.then(createChart)
-.catch(error => {
-    console.error("Error fetching data:" + error);
-});
+export { authSignOut, isLoggedIn }
